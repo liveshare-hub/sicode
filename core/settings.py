0c876fe17ea513cdc4100ea10b88949dd97aa891
s4-hub <syafii.newbie@gmail.com>
2021-11-08 22:15:33 +0700

    fixing bugs

--- core/settings.py
+++ core/settings.py
@@ -85,10 +85,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('name_db'),
+        'USER': config('user_db'),
+        'PASSWORD': config('pass_db'),
+        'HOST': config('host_db'),
+        'PORT': '',
     }
 }
 
