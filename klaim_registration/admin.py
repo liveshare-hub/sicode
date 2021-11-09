09eccf3189868094f9de4e22d74b2656a1441ef3
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-09 10:05:38 +0700

    fixing

--- klaim_registration/admin.py
+++ klaim_registration/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from .models import DataTK, ApprovalHRD, toQRCode, KPJ, SebabKlaim, TipeKlaim
+from .models import DataTK, ApprovalHRD, toQRCode, KPJ, SebabKlaim, TipeKlaim, DataKlaim
 
 admin.site.register(DataTK)
 admin.site.register(KPJ)
 admin.site.register(SebabKlaim)
 admin.site.register(TipeKlaim)
-# admin.site.register(DaftarHRD)
+admin.site.register(DataKlaim)
 admin.site.register(ApprovalHRD)
 admin.site.register(toQRCode)
