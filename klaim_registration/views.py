3aee6c37863701fa24ae12c01cdc85a1a5f80cbe
s4-hub <syafii.newbie@gmail.com>
2021-11-12 07:43:49 +0700

    fixing klaim menu

--- klaim_registration/views.py
+++ klaim_registration/views.py
@@ -37,10 +37,9 @@ from email import encoders
 def index(request):
 
     datas = DataTK.objects.select_related('hrd').filter(hrd=request.user.profile).annotate(data_kpj=Subquery(KPJ.objects.filter(
-        data_tk=OuterRef('pk'), is_aktif=True).values('no_kpj').values('pk')))
+        data_tk=OuterRef('pk'), is_aktif=True).values('no_kpj')))
 
     # datas = DataTK.objects.select_related('hrd').filter(hrd=request.user.profile).all()
-    print(datas[0].data_kpj.no_kpj)
     context = {
         'datas': datas
     }
