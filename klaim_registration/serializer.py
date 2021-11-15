121e7f5134481cf468f4ea353fd79ee58b664f4e
s4-hub <syafii.newbie@gmail.com>
2021-11-15 15:00:30 +0700

    create klaim

--- klaim_registration/serializer.py
+++ klaim_registration/serializer.py
@@ -2,8 +2,14 @@ from rest_framework import serializers
 from .models import KPJ, DataTK
 
 
+class DataTKSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataTK
+        fields = ['id', 'nama']
+
+
 class KPJSerializer(serializers.ModelSerializer):
-    data_tk = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    data_tk = DataTKSerializer(many=True, read_only=True)
 
     class Meta:
         model = KPJ
