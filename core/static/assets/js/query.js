ddd6c4444f92ddbab4b827ddd8a2c415a465ca3e
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-16 11:18:18 +0700

    fixing query

--- core/static/assets/js/query.js
+++ core/static/assets/js/query.js
@@ -18,7 +18,7 @@ $("#id_kpj").focusout(function() {
         success:function(data){
             $(this).attr("disabaled", true);
             var nama = data['allKpjs']
-            console.log(nama)
+            console.log(data)
         },
         error:function(err){
             console.log(err)
