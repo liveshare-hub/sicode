1bd8dad7c470aa146d2ca1bb669f793767033cd0
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-16 13:24:55 +0700

    fixing query

--- core/static/assets/js/query.js
+++ core/static/assets/js/query.js
@@ -4,39 +4,31 @@ var superQuery = `query ($kpj: String!) {allKpjs(noKpj:$kpj){
     }
 }}`
 
-function tc(func, msg){
-    msg = msg || "Handler Exception"
-    return function(e) {
-        try{
-            return func(e)
-        }
-        catch (exc){
-            $("#id_nama").val("FORMAT KPJ SALAH!!!");
-            throw exc;
-        }
-    }
-}
 
-$("#id_kpj").focusout(tc(function(e) {
+$("#id_kpj").focusout(function() {
     var kpj = $(this).val()
-    $.ajax({
-        method:"POST",
-        url:"https://sicode.id/graphql",
-        contentType:"application/json",
-        data: JSON.stringify({
-            query:superQuery,
-            variables: {"kpj":kpj}
-        }),
-        success:function(data){
-            $(this).attr("disabled", true);
-            var nama = data['data']['allKpjs'][0]['dataTk']['nama']
-            $("#id_nama").val(nama)
-
-        },
-        error:function(err){
-            console.log(err)
-            $("#id_nama").val("Format KPJ SALAH!!!")
-        }
-    })
-}, "FORMAT KPJ SALAH"));
+    if($(this).length !== 11){
+        $("#id_nama").val("FORMAT KPJ SALAH!")
+    }else{
+        $.ajax({
+            method:"POST",
+            url:"https://sicode.id/graphql",
+            contentType:"application/json",
+            data: JSON.stringify({
+                query:superQuery,
+                variables: {"kpj":kpj}
+            }),
+            success:function(data){
+                $(this).attr("disabled", true);
+                var nama = data['data']['allKpjs'][0]['dataTk']['nama']
+                $("#id_nama").val(nama)
+    
+            },
+            error:function(err){
+                console.log(err)
+                $("#id_nama").val("Format KPJ SALAH!!!")
+            }
+        })
+    }
+});
 // console.log(kpj)
