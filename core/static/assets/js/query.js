071f359a788deae7adf58736c378f112d0008b95
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-16 11:17:12 +0700

    fixing query

--- core/static/assets/js/query.js
+++ core/static/assets/js/query.js
@@ -17,7 +17,7 @@ $("#id_kpj").focusout(function() {
         }),
         success:function(data){
             $(this).attr("disabaled", true);
-            var nama = data[0]
+            var nama = data['allKpjs']
             console.log(nama)
         },
         error:function(err){
