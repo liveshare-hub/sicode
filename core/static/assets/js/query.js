7bf58ed9dc1ffbb4940eafd5d18bbe3c20b96f1d
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-16 15:38:09 +0700

    fixing query

--- core/static/assets/js/query.js
+++ core/static/assets/js/query.js
@@ -28,7 +28,7 @@ $("#id_kpj").focusout(function() {
                     var nik = dataNama[0]['dataTk']['nik']
                     $("#id_nama").val(nama)
                     $("#id_nik").val(nik)
-                    $("#id_kpj").attr("disabled",true)
+                    $("#id_kpj").attr("disabled",true).val(kpj)
                 }else{
                     $("#id_nama").val("KPJ TIDAK DITEMUKAN")
                     $("#id_nik").val("KPJ TIDAK DITEMUKAN")
