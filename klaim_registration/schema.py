cbdaafbe06dcf16a97a674b5ac8b2815668f7c66
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-16 10:30:39 +0700

    fixing query

--- klaim_registration/schema.py
+++ klaim_registration/schema.py
@@ -18,7 +18,7 @@ class Query(graphene.ObjectType):
     all_kpjs = graphene.List(KPJType, no_kpj=graphene.String())
     all_tk = graphene.List(DataTKType)
 
-    def resolve_all_kpjs(root, info, no_kpj, nik):
+    def resolve_all_kpjs(root, info, no_kpj):
 
         if info.context.user.is_authenticated or info.context.user.is_superuser:
             return KPJ.objects.select_related('data_tk').filter(no_kpj=no_kpj)
