e86debb16e439caebecfef797232e13174747cec
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-16 10:53:29 +0700

    fixing query

--- core/static/assets/js/query.js
+++ core/static/assets/js/query.js
@@ -1,6 +1,30 @@
 
 $("#id_kpj").focusout(function() {
     var kpj = $(this).val()
-    console.log(kpj)
+    $.ajax({
+        method:"POST",
+        url:"https://sicode.id/graphql",
+        contentType:"application/json",
+        data: JSON.stringify({
+            query: `{
+                allKpjs(noKpj:${kpj}){
+                  noKpj
+                  tglKeps
+                  tglNa
+                  isAktif
+                  dataTk{
+                    nik
+                    nama
+                  }
+                }
+              }`
+        }),
+        success:function(data){
+            console.log(data)
+        },
+        error:function(err){
+            console.log(err)
+        }
+    })
 })
 // console.log(kpj)
