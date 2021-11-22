b4eb0891bd056a9a6be3c6da637761cb2b657d28
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-22 15:09:51 +0700

    fixing ajax

--- klaim_registration/models.py
+++ klaim_registration/models.py
@@ -183,8 +183,8 @@ class DataKlaim(models.Model):
 @receiver(post_save, sender=DataKlaim)
 def Approval(sender, instance, created, **kwargs):
     if created:
-        # print(instance.no_kpj.data_tk.hrd.pk)
-        print(kwargs)
+        print(instance.no_kpj.data_tk.hrd.user_id)
+        # print(kwargs)
         ApprovalHRD.objects.create(klaim=instance, hrd=instance.no_kpj.data_tk.hrd.user_id)
 
 
