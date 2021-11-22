e411f1316974e0f879cc5642af6bf28fb7ce82c4
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-22 08:31:45 +0700

    fixing ajax

--- klaim_registration/models.py
+++ klaim_registration/models.py
@@ -169,7 +169,7 @@ class DataKlaim(models.Model):
     flag_klaim = models.BooleanField(default=False)
 
     def __str__(self):
-        return '{} - {}'.format(self.no_kpj.no_kpj, self.no_kpj.nama)
+        return '{} - {}'.format(self.no_kpj.no_kpj, self.no_kpj.data_tk.nama)
 
     def save(self, *args, **kwargs):
         self.flag_klaim = True
