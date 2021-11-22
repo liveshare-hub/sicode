60d89bc025949707bfc600fbfd546f3aaa7b263b
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-22 13:32:44 +0700

    fixing ajax

--- core/static/assets/js/klaimAjax.js
+++ core/static/assets/js/klaimAjax.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
                'csrfmiddlewaretoken':$("input[name='csrfmiddlewaretoken']").val()
            },
            dataType:"json",
+           processData:false,
            success:function(data){
                console.log(data)
            },
