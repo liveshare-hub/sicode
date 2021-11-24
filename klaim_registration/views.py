89d3b51bc7c6d30b70468330c41040b98076dca9
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-24 15:50:28 +0700

    fixing qrcode

--- klaim_registration/views.py
+++ klaim_registration/views.py
@@ -317,8 +317,8 @@ def sent_mail(request, pk):
     tk_id = ApprovalHRD.objects.select_related('klaim','hrd').get(pk=pk)
     qrcode = tk_id.img_svg
     to = tk_id.klaim.no_kpj.data_tk.email
-    im = Image.open(qrcode.file)
-    print(qrcode.url)
+    im = Image.open(qrcode.url)
+    print(dir(qrcode))
     bg = Image.new("RGB", (450,450), "white")
     bg.paste(im, (0,0), im)
     bg.save(qrcode.name + ".jpg", quality=95)
