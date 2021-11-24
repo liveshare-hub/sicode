574e21aabf0eda5ccf64ee6d3c8ee8fa24918e0d
liveshare-hub <syafii.newbie2@gmail.com>
2021-11-24 09:07:30 +0700

    fixing icon

--- core/static/assets/js/showChildTable.js
+++ core/static/assets/js/showChildTable.js
@@ -4,7 +4,7 @@ function format ( d ) {
     return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
         '<tr>'+
             '<td>Paklaring:</td>'+
-            '<td>'+d.klaim__parklaring+'</td>'+
+            '<td>'+d.0.klaim__parklaring+'</td>'+
         '</tr>'+
         '<tr>'+
             '<td>Extension number:</td>'+
