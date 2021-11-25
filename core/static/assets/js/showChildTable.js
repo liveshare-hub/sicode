5654297ffe259f582343e16fdc72e6a00b9b5ca9
s4-hub <syafii.newbie@gmail.com>
2021-11-25 15:22:01 +0700

    fixing table

--- core/static/assets/js/showChildTable.js
+++ core/static/assets/js/showChildTable.js
@@ -10,7 +10,7 @@ function GetData(datas) {
 function format ( d ) {
     // `d` is the original data object for the row
     var items = GetData(d)
-    console.log(items[0].responseJSON)
+    console.log(items.responseJSON)
     var m = JSON.stringify(items)
     console.log(m)
     return (
