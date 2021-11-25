37575da41dc59448774196a5a242d00ef39935d4
s4-hub <syafii.newbie@gmail.com>
2021-11-25 13:34:13 +0700

    fixing table

--- core/static/assets/js/showChildTable.js
+++ core/static/assets/js/showChildTable.js
@@ -2,15 +2,7 @@
 function format ( d ) {
     // `d` is the original data object for the row
     console.log(d)
-    var id = $("#klaim_id").val()
-    $.ajax({
-        url:`/ajax/tk/${d}`,
-        type:'GET',
-        success:function(res){
-            console.log(res)
-            return '<div>Hidden Value: ' + res + '</div>'
-        }
-    })
+    
 }
 //     return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
 //         '<tr>'+
