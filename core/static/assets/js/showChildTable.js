d6178c59e1cae79da22339ab4d042caa5af1ca30
s4-hub <syafii.newbie@gmail.com>
2021-11-25 14:52:40 +0700

    fixing table

--- core/static/assets/js/showChildTable.js
+++ core/static/assets/js/showChildTable.js
@@ -10,7 +10,7 @@ function format ( d ) {
     })
     return (
         $.each(items, function(i, item){
-            console.log(item)
+            console.log(i)
         '<table cellpadding="5" cellspacing="0" style="padding-left:50px;">'+
         '<tr>'+
         '<td>Paklaring:</td>'+
