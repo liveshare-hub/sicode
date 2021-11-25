b46fa064e715eeeba3f703d001b358282a345e77
s4-hub <syafii.newbie@gmail.com>
2021-11-25 13:58:16 +0700

    fixing table

--- core/static/assets/js/showChildTable.js
+++ core/static/assets/js/showChildTable.js
@@ -1,7 +1,15 @@
 /* Formatting function for row details - modify as you need */
 function format ( d ) {
     // `d` is the original data object for the row
-    return '<div>Hidden Value: ' + d + '</div>';
+    return (
+        $.ajax({
+            url:`/ajax/tk/${d}`,
+            type:'GET',
+            success:function(data){
+                console.log(data)
+            }
+        })
+    )
     
 }
 //     return '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">'+
